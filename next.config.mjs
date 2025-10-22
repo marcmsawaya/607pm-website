b5a7f56afbdefa496ebd2b6872c59cc7a5f29462
Marc Sawaya <sawaya.m@northeastern.edu>
2025-10-22 00:15:23 -0400

    removed react compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
   experimental: {
     turbo: {
       root: '/Users/unknownfriend/607pm-website'
