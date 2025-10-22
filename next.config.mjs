d0bcd65f63d3e3e8ccb530e940c2918e75edb115
Marc Sawaya <sawaya.m@northeastern.edu>
2025-10-22 00:12:10 -0400

    ttekil

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    turbo: {
+      root: '/Users/unknownfriend/607pm-website'
+    }
+  }
 };
 
 export default nextConfig;
